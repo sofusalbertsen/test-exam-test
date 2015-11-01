b8dc4a8f7086bba722c522616c85d24838eeeb83
sofus <sofusalbertsen@gmail.com>
2015-11-01 17:12:53 +0100

    Added LAM's helping factory in View5 as well as table creation

--- AngTestClient/app/view5/view5.js
+++ AngTestClient/app/view5/view5.js
@@ -1,30 +1,46 @@
 'use strict';
 
+
+
 angular.module('myApp.view5', ['ngRoute'])
 
+
+
         .config(['$routeProvider', function ($routeProvider) {
+
                 $routeProvider.when('/view5', {
                     templateUrl: 'view5/view5.html',
                     controller: 'View5Ctrl'
-                });
-            }])
 
+                });
+
+            }])
         .controller('View5Ctrl', function ($scope, UserFactory) {
-            $scope.data = UserFactory.fetchUsers();
+
+            UserFactory.fetchUsers().then(
+                    function OK(response) {
+
+                        $scope.data = response.data;
+                    }, function notOK() {
+                //DO something here
+            }
+            );
             console.log(UserFactory);
+
         })
-        .factory('UserFactory', ['$http', function ($http) {       
-                           return({fetchUsers: this.fetchUsers});
-                function fetchUsers () {
-                        $http.get('api/demoadmin/user')
-                                .success(function (data, status, headers, config) {
-//                                 console.log(data);
-                                            return data;
-                                })
-                                .error(function (data, status, headers, config) {
-                                    return data;
-                                });
-                    };
-                
+
+        .factory('UserFactory', ['$http', function ($http) {
+
+                return({
+                    fetchUsers: fetchUsers
+
+                });
+
+                function fetchUsers() {
+
+                    return $http.get('api/demoadmin/user');
+
+                };
             }]);
+
 ;
